c4a2f00e6cd5295dd438999ffdfd9764dd46f46c
Cadence Eva Custin <eva.custin@gmail.com>
2023-07-25 15:38:08 -0400

    initial code and write up

--- js/site.js
+++ js/site.js
@@ -1,13 +1,35 @@
-function displayMessage() {
-    let inputBox = document.getElementById("message");
+function getValues() {
+    let userString = document.getElementById('userString').value;
+    let reversedString = reverseWithRecursion(userString);
+    displayString(reversedString);
+}
 
-    let message = inputBox.value;
+function reverseWithRecursion(userString, reversedString = '') {
+    return userString.length == reversedString.length ? reversedString : reverseWithRecursion(userString, userString[reversedString.length] + reversedString);
+}
 
-    Swal.fire(
-        {
-            backdrop: false,
-            title: 'App Name',
-            text: message
-        }
-    );
+function basicReverseString(userString) {
+    let reversedString = '';
+    for (let i = userString.length - 1; i >= 0; i--) reversedString += userString[i];
+    return reversedString;
+}
+
+function reverseWithCharAt(userString) {
+    let reversedString = '';
+    for (let i = userString.length - 1; i >= 0; i--) reversedString += userString.charAt(i);
+    return reversedString;
+}
+
+function reverseWithArray(userString) {
+    let userArray = userString.split('');
+    let reversedArray = [];
+    let reversedString = '';
+    for (let i = 0; i < userArray.length; i++) reversedArray.unshift(userArray[i]);
+    for (let i = 0; i < reversedArray.length; i++) reversedString += reversedArray[i];
+    return reversedString;
+}
+
+function displayString(reversedString) {
+    document.getElementById('results').textContent = reversedString;
+    document.getElementById('alert').classList.remove('invisible')
 }
